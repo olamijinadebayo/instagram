481f7a5d7fe2f650c01cb54bf148b07cb1d20b9a
olamijinadebayo <consultolamijin@gmail.com>
2018-05-14 06:31:19 +0300

    This will add form classes to send welcome mails,post new images,rdit user details and edit profile

---
+++ instagram/insta_clone/forms.py
@@ -0,0 +1,27 @@
+from django import forms
+from .models import Comment, Profile, Image, User
+
+
+class WelcomeEmailForm(forms.Form):
+    your_name = forms.CharField(label='First Name', max_length=30)
+    email = forms.EmailField(label='Email')
+
+
+class NewImagePost(forms.ModelForm):
+    class Meta:
+        model = Image
+        fields = ['image', 'image_name', 'image_caption']
+        exclude = ['profile, user, Likes, comments, tags']
+
+
+class EditUserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email')
+
+
+class EditProfile(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['avatar', 'bio']
+        exclude = ['user']

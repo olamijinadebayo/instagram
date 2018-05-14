0e9c4c0805222b3d1f656722417929bce29c2fbc
olamijinadebayo <consultolamijin@gmail.com>
2018-05-14 06:44:05 +0300

    This will create an email script that will handle the sending of mails and also create two templates containing the email body.

--- instagram/insta_clone/views.py
+++ instagram/insta_clone/views.py
@@ -3,10 +3,30 @@ from django.shortcuts import render
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from .models import Image, Profile
 from django.contrib.auth.decorators import login_required
-#from .email import send_welcome_email
+from .email import send_welcome_email
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+
+def home(request):
+    images = Image.objects.all()
+    profiles = Profile.objects.all()
+    current_user = request.user
+    if request.method == 'POST':
+        form = WelcomeEmailForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['your_name']
+            email = form.cleaned_data['email']
+            recipient = WelcomeEmailRecipients(name=name, email=email)
+            recipient.save()
+            send_welcome_email(name, email)
+
+            HttpResponseRedirect('/accounts/login/')
+    else:
+        form = WelcomeEmailForm()
+    context = {"images": images, "current_user": current_user, "form": form, "profiles": profiles}
+    return render(request, 'index.html', context)
 
 
 def explore(request):
